1c06fa1ba71ca3d96bb9d8fb636473716c9f7812
Joseph <inboxjed@gmail.com>
2019-04-16 16:06:00 -0400

    Greets user and draws blanks

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-const words = [ "document object model", "object", "method", "buisness soultion", "rate of return", "capitalize"];
+$(document).ready(() => {
+  $('#blankRow').hide();
+  function getWord (){
+  const words = [ "dom", "object", "method", "array", "traverse", "value"];
+  let randomNum = Math.floor(Math.random() * words.length);
+  let randomWord = words[randomNum];
+  return randomWord
+}
+let currWord = getWord();
+let blanks = "";
+for(let i = 1; i <= currWord.length; i++){
+  blanks += " _"
+}
+$(blankRow).html(blanks);
+
+$('#getstarted').hide();
+$('#name').on('keyup',() =>{
+$('#getstarted').show();
+});
+$('#continue').on('click', ()=> {
+
+    $('#nameForm').hide();
+    const inputValue = $('#name').val();
+    $('#greeting').html('Hello, '+ inputValue)
+    $('#blankRow').show();
+})
+
+
+});
