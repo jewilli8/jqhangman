47ce28834f497ecef0f862d06f7ebee2cff60881
Joseph <inboxjed@gmail.com>
2019-04-23 15:49:10 -0400

    Gets user Input

--- script.js
+++ script.js
@@ -1,29 +1,67 @@
 $(document).ready(() => {
   $('#blankRow').hide();
+  $('#letterInputform').hide();
+  $('#lettersInRow').hide();
   function getWord (){
-  const words = [ "dom", "object", "method", "array", "traverse", "value"];
+  const words = [ "business", "object", "kale", "pineapple", "friend", "penguin"];
   let randomNum = Math.floor(Math.random() * words.length);
   let randomWord = words[randomNum];
   return randomWord
 }
 let currWord = getWord();
+let currWordArray = currWord.split("")
+let answerArray = []
+
+function drawBlanks(){
+
 let blanks = "";
 for(let i = 1; i <= currWord.length; i++){
   blanks += " _"
 }
 $(blankRow).html(blanks);
+}
+
+
+
+console.log(currWordArray)
+console.log(answerArray)
 
 $('#getstarted').hide();
-$('#name').on('keyup',() =>{
+$('#name').on('mouseenter',() =>{
 $('#getstarted').show();
 });
 $('#continue').on('click', ()=> {
 
     $('#nameForm').hide();
     const inputValue = $('#name').val();
-    $('#greeting').html('Hello, '+ inputValue)
+    $('#greeting').html('Hello, '+ inputValue);
+    drawBlanks();
     $('#blankRow').show();
-})
+    $('#letterInputform').show();
+    $('#lettersInRow').show();
+
+
+});
+$('#submitGuess').on('click', () => {
+  let currGuess = $('#guessInput').val();
+  let numberCheck = Number(currGuess);
+  if ((Number.isNaN(numberCheck)) && (currGuess.length === 1)){
+    if (currWord.includes(currGuess)){
+    let letterPosition = currWordArray.indexOf(currGuess)
+     answerArray[letterPosition] = currGuess;
+     console.log(answerArray)
+
+    } else {
+      alert('letter not included')
+    }
+
+  }
+  else {
+    $('<div class="alert alert\-danger " role="alert"> Please enter a single letter to continue.</div>')
+    .insertBefore('#letterInputform')
+  }
+});
+
 
 
 });
